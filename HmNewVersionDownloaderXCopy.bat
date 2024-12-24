504c32c0391521a879329c0f8cd4e1072b93eaf9
komiyamma <komiyama0629@gmail.com>
2024-12-25 03:21:25 +0900

    こみっと

--- HmNewVersionDownloaderXCopy.bat
+++ HmNewVersionDownloaderXCopy.bat
@@ -6,6 +6,10 @@ taskkill /f /im hidemaru.exe
 timeout /t 3 /nobreak >nul
 
 xcopy "%~1" "%~2" /s /e /y
+if errorlevel 1 (
+    echo xcopy failed, attempting to run as administrator...
+    powershell -Command "Start-Process cmd -ArgumentList '/c xcopy \"%~1\" \"%~2\" /s /e /y' -Verb RunAs"
+)
 
 pushd "%~2"
 start hidemaru.exe /x"%~3" /a"show_release_note"
